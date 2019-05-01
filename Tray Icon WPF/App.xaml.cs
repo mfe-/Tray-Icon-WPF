dee2fb91ea137941b70bef03acd74c94431f4ab1
Martin Fetzel <martin.fetzel@get-the-solution.net>
2019-05-01 21:22:39 +0200

    add

---
+++ Tray Icon WPF/App.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+using System.Drawing;
+
+namespace Tray_Icon_WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            this.ShutdownMode = ShutdownMode.OnExplicitShutdown;
+
+            //Icon laden
+            //Achtung Pfad zum Icon anpassen!!!!
+            Icon icon = Icon.ExtractAssociatedIcon(@"C:\Users\martin\Documents\Visual Studio 2008\Blog\Tray Icon WPF\Tray Icon WPF\Icon1.ico"); 
+
+            //trayicon erstellen und icon laden
+            Tray tray = new Tray(icon);
+
+            tray.CreateMenuItem("Fenster aufrufen");
+            tray.CreateMenuItem("Beenden");
+
+            //Menuitem Fenster aufrufen suchen, zugreifen Click Event anmelden und delegate setzen
+            tray.NotifyIcon.ContextMenu.MenuItems.Find("Fenster aufrufen", true).First().Click += (sender, eargs) =>
+            {
+                Window1 window = new Window1();
+                window.Show();
+
+            };
+
+            //Menuitem Beenden suchen, zugreifen Click Event anmelden und delegate setzen
+            tray.NotifyIcon.ContextMenu.MenuItems.Find("Beenden", true).First().Click += (sender, eargs) =>
+            {
+                //Icon aus tray löschen
+                tray.NotifyIcon.Dispose();
+                Environment.Exit(0);
+            };
+
+
+            base.OnStartup(e);
+        }
+    }
+}
---
+++ Tray Icon WPF/Tray.cs
@@ -0,0 +1,67 @@
+﻿using System.Windows.Forms;
+using System;
+
+public class Tray
+{
+    private NotifyIcon _notico;
+    private bool _Animate = false;
+    private ContextMenu _contextMenu = new ContextMenu();
+
+    public Tray()
+    {
+        Initialize();
+    }
+    public Tray(System.Drawing.Icon pIcon)
+    {
+        Initialize();
+        _notico.Icon = pIcon;
+    }
+    /// <summary>
+    /// Initialisiert das NotifyIcon
+    /// </summary>
+    private void Initialize()
+    {
+        // NotifyIcon erzeugen
+        _notico = new NotifyIcon();
+        _notico.Visible = true;
+
+        ContextMenu contextMenu = new ContextMenu();
+
+        // Kontextmenüeinträge erzeugen
+
+        _notico.ContextMenu = _contextMenu;
+
+    }
+    public void CreateMenuItem(String pName)
+    {
+        MenuItem menuItem = new MenuItem();
+        menuItem = new MenuItem();
+        menuItem.Index = 1;
+        menuItem.Name = pName;
+        menuItem.Text = "&" + menuItem.Name;
+
+        _contextMenu.MenuItems.Add(menuItem);
+    }
+    public void CreateMenuItem(String pName, bool pTrue)
+    {
+        MenuItem menuItem = new MenuItem();
+        menuItem.Index = 2;
+        menuItem.Name = pName;
+        menuItem.Text = "&" + menuItem.Name;
+        menuItem.Click += (sender, e) =>
+        {
+            MenuItem m = (MenuItem)sender;
+            m.Checked = !m.Checked;
+        };
+        menuItem.Checked = pTrue;
+
+        _contextMenu.MenuItems.Add(menuItem);
+    }
+    public NotifyIcon NotifyIcon
+    {
+        get
+        {
+            return _notico;
+        }
+    }
+}
